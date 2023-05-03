0d669d0b15d849a53c3a0d508f4534df145e74a8
Bob Rojas <bobatthevelvetroom@gmail.com>
2023-05-03 11:29:07 -0400

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
 # your code goes here!
+def countdown():
+    x = 10
+    while x != 0:
+        if x != 0 :
+            print(f'{x} SECOND(S)!')
+            x -= 1
+        if x == 0:
+            print("HAPPY NEW YEAR")
+
+countdown()
